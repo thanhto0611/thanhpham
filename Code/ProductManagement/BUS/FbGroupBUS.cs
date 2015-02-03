b805818ef42d589c179c476afa7b39231d0a810e
thanh.pham611@gmail.com <thanh.pham611@gmail.com@80c3de3b-1e1d-71be-48d5-a87c6c997a35>
2015-02-03 11:34:25 +0000

    Add Save groups & Update groups

---
+++ Code/ProductManagement/BUS/FbGroupBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Data;
+using System.Data.OleDb;
+using DTO;
+using DAO;
+
+namespace BUS
+{
+    public class FbGroupBUS
+    {
+        public static void Insert(FbGroupDTO group)
+        {
+            FbGroupDAO gd = new FbGroupDAO();
+            gd.Insert(group);
+        }
+
+        public static void Update(FbGroupDTO group)
+        {
+            FbGroupDAO gd = new FbGroupDAO();
+            gd.Update(group);
+        }
+    }
+}

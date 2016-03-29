f3de5d6c500695d41a65dcdf6301153469faeaf6
thanhto0611 <thanh.pham611@gmail.com>
2016-03-29 13:50:51 +0700

    delete adeptra

--- Code/Adeptra/Adeptra_Tools/lloydsCredit_files/filter-tooltip.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#portfolioFilter").on("mouseenter", "div[rel=tooltip] > input:not(.hasTooltip)", function(){
-		$(this).tooltip({animation: true, placement: "bottom", delay: {show: 250, hide: 100}}).addClass("hasTooltip");
-		$("#portfolioFilter").off("mouseenter", "div[rel=tooltip] > input.hasTooltip");
-		$(this).trigger("mouseenter");
-	});
-});
